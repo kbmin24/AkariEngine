09b9954828558997a65384048f404825a37149bf
Kyubin Min <57584060+kbmin24@users.noreply.github.com>
2022-03-08 18:22:03 +0900

    a

--- src/AJAX/recentchanges.js
+++ src/AJAX/recentchanges.js
@@ -21,7 +21,8 @@ module.exports = async (req, res, recentchanges) =>
     changes.forEach((value, index, array) =>
     {
         if (show <= 0) return
-        if (excludefile && value.page.toLowerCase().startsWith('file:')) return
+        if (excludefile && value.page.toLowerCase().startsWith('File:')) return
+        if (excludefile && value.page.toLowerCase().startsWith('User:')) return
         if (editOnly && value.type !== 'edit') return
         if (!isUnique || !uniqueNames.has(value.page))
         {
